efc22c71355cdca78cf7a3f0f5bade60e2367006
Yamahage <seancholland@gmail.com>
2017-04-12 14:47:45 -0600

    Add files via upload

---
+++ Picross Project/Part 1/gameState.js
@@ -0,0 +1,7 @@
+gameState: {
+  "grid": [[x,0,0,b,x], [x,0,0,b,x],[x,0,0,b,x],[x,0,0,b,x],[x,b,b,b,x]],
+  "row labels":[[1],[3], [1 1], [3], [3]],
+  "column labels":[0,4, 1 2, 5, 0],
+  "brush": b,
+  "puzzleComplete": false
+  }
