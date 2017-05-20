230d3798fb566796d8faf6f3363794d6da631d10
Sean Holland <seancholland@gmail.com>
2017-05-19 18:33:30 -0600

    Edit CSS

--- Picross Project/Part 1/gameState.js
+++ Picross Project/Part 1/gameState.js
@@ -1,7 +1,7 @@
 gameState: {
-  "grid": [[x,0,0,b,x], [x,0,0,b,x],[x,0,0,b,x],[x,0,0,b,x],[x,b,b,b,x]],
+  "grid": [[x,0,0,c,x], [x,0,0,c,x],[x,0,0,c,x],[x,0,0,c,x],[x,c,c,c,x]],
   "row labels":[[1],[3], [1 1], [3], [3]],
   "column labels":[0,4, 1 2, 5, 0],
-  "brush": b,
-  "puzzleComplete": false
+  "brush": c, x
+  "finish": false
   }
